4d5e1c6d70b45531d6e251c0640b9fe51e2ab3a0
Andre <andreyamasaki@brq.com>
2021-07-02 17:03:49 -0300

    added segue connection between views

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -18,6 +18,7 @@ class CalculatorViewController: UIViewController {
     
     var tipPercentage = 0.10
     var numberOfPeople = 2
+    var total = "0.0"
     
     @IBAction func tipChanged(_ sender: UIButton) {
         zeroPctButton.isSelected = false
@@ -43,8 +44,17 @@ class CalculatorViewController: UIViewController {
         let bill = billTextField.text!
         guard let doubleBill = Double(bill) else { return }
         let totalBill = doubleBill * (1 + tipPercentage) / Double(numberOfPeople)
-        print(String(format: "%.2f", totalBill))
-        
+        total = String(format: "%.2f", totalBill)
+        performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.split = numberOfPeople
+            destinationVC.tip = tipPercentage
+            destinationVC.total = total
+        }
     }
     
 }
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
+    var total = "0.0"
+    var tip = 0.10
+    var split = 2
+    
+    
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        totalLabel.text = String(total)
+        settingsLabel.text = "Split between \(split) people, with \(100 * tip)% tip."
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
-    
-
 }
